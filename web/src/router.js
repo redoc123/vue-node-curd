1156b192e2730afd45e64b2884b414fc77a5c418
Bhanu Nemani <bhanu.n@vigocare.com>
2019-07-16 01:03:32 +0530

    removed stock home

--- web/src/router.js
+++ web/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import UserList from './views/users/index.vue'
 import User from './views/users/show.vue'
 
@@ -10,11 +9,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/users',
       name: 'userList',
